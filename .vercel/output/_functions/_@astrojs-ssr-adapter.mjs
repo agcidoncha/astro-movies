9d44cabca556ca7628e647c2b261e034eca31e56
cidonchadev <cidoncha@ifyoulikecars.com>
2026-03-17 17:48:17 +0100

    remove vercel build files

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_B90wu9H2.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_LYEoQMgs.mjs';
-export { renderers } from '../renderers.mjs';
